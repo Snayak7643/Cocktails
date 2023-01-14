32b5731103e532ebfd5879cdbb26a00d587394d1
Snayak7643 <shubhamnayak7643@gmail.com>
2023-01-14 14:06:17 +0530

    search implemented

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import CocktailContext from "../Contexts/cocktailContext";
 
 const Search = () => {
+  const { searchTerm, handleChange } = useContext(CocktailContext);
+
   return (
     <section className="section search">
       <form className="search-form">
         <div className="form-control">
           <label>search your favorite cocktail</label>
-          <input type="text" />
+          <input type="text" value={searchTerm} onChange={handleChange} />
         </div>
       </form>
     </section>

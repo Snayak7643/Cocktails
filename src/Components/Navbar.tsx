5ff205971cb626fe0c706e966e2a58c40ff7acc3
Snayak7643 <shubhamnayak7643@gmail.com>
2023-01-16 15:19:02 +0530

    icons added in navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FcFullTrash } from "react-icons/fc";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="nav-center">
-        <img src="" alt="cocktail db logo" className="logo" />
+        <h2>
+          The
+          <FcFullTrash />
+          Shop
+        </h2>
         <ul className="nav-links">
           <Link to="/">
             <li>Home</li>
@@ -14,6 +19,14 @@ const Navbar = () => {
             <li>About</li>
           </Link>
         </ul>
+        <div className="nav-container">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M16 6v2h2l2 12H0L2 8h2V6a6 6 0 1 1 12 0zm-2 0a4 4 0 1 0-8 0v2h8V6zM4 10v2h2v-2H4zm10 0v2h2v-2h-2z" />
+          </svg>
+          <div className="amount-container">
+            <p className="total-amount">0</p>
+          </div>
+        </div>
       </div>
     </nav>
   );

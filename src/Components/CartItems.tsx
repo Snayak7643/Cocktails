48e4a7856a982b7654d1d90489f91a203d80fb92
Snayak7643 <shubhamnayak7643@gmail.com>
2023-01-18 11:36:32 +0530

    link added in cart

--- src/Components/CartItems.tsx
+++ src/Components/CartItems.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { FcExpand, FcCollapse } from "react-icons/fc";
 import CartContext from "../Contexts/cartContext";
 import { Increase, Decrease, Remove } from "../Reducers/actions";
@@ -18,7 +19,10 @@ const CartItem = () => {
         return (
           <div className="item-container" key={id}>
             <img src={img} alt="cocktail" />
-            <h3>{name}</h3>
+            <div className="item-heading">
+              <h3>{name}</h3>
+              <Link to={"/cocktail/" + id}>Details</Link>
+            </div>
             <div className="cart-btn">
               <button
                 onClick={() => {
